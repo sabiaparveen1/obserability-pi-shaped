cec0fd7f41fbc1753f7b86197db4e116efa8f851
sabiaparveen1 <sabia.parveen@nashtechglobal.com>
2026-02-03 14:09:19 +0530

    initial setup

---
+++ src/main/java/com/observaibility/observability_order_service/ObservabilityOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.observaibility.observability_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/observaibility/observability_order_service/ObservabilityOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.observaibility.observability_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
